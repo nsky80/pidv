4a28adf2ccf3c9028686f48b54523d0b22da38f9
Satish NSKY80 <satishkumary80@gmail.com>
2020-04-25 20:14:12 +0530

    static files added

--- pidv/user_mgmt/views.py
+++ pidv/user_mgmt/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello Old friend")
+
+
+# This function returns welcome screeen
+def homepage(request):
+	return render(request=request, 
+				  template_name="user_mgmt/homepage.html",
+				 )	
